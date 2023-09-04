1d02d9d76d987b69ed08a252a3fa66f7d05a4cbc
TheArcaneBrony <myrainbowdash949@gmail.com>
2023-09-04 02:16:40 +0200

    Clean up some extension functions

--- LibMatrix/Extensions/IEnumerableExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace LibMatrix.Extensions;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
-public class MatrixEventAttribute : Attribute {
-    public string EventName { get; set; }
-    public bool Legacy { get; set; }
-}
