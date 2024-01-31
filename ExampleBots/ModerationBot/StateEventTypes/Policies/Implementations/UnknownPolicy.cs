d133a168c17d605236e967ff63728420ddb0ed98
Rory& <root@rory.gay>
2024-01-31 18:50:16 +0100

    Repo split

--- ExampleBots/ModerationBot/StateEventTypes/Policies/Implementations/UnknownPolicy.cs
+++
@@ -1,7 +0,0 @@
-namespace ModerationBot.StateEventTypes.Policies.Implementations;
-
-/// <summary>
-///     Unknown policy event, usually used for handling unknown cases
-/// </summary>
-public class UnknownPolicy : BasePolicy {
-}
--- ExampleBots/PluralContactBotPoC/Bot/PluralContactBotConfiguration.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Extensions.Configuration;
-
-namespace PluralContactBotPoC.Bot;
-
-public class PluralContactBotConfiguration {
-    public PluralContactBotConfiguration(IConfiguration config) => config.GetRequiredSection("PluralContactBot").Bind(this);
-
-    // public string
-}
