65e52735bd7b9ae249b97e807d0c83d4e6d6570a
Rory& <root@rory.gay>
2024-06-17 00:43:25 +0200

    Single httpclient implementation

--- LibMatrix/Extensions/MatrixHttpClient.Single.cs
+++ LibMatrix/Extensions/MatrixHttpClient.Single.cs
@@ -1,8 +1,10 @@
+#define SINGLE_HTTPCLIENT // Use a single HttpClient instance for all MatrixHttpClient instances
+// #define SYNC_HTTPCLIENT // Only allow one request as a time, for debugging
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Net.Http.Headers;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -11,27 +13,51 @@ using ArcaneLibs.Extensions;
 
 namespace LibMatrix.Extensions;
 
-public static class HttpClientExtensions {
-    public static async Task<bool> CheckSuccessStatus(this HttpClient hc, string url) {
-        //cors causes failure, try to catch
+#if SINGLE_HTTPCLIENT
+public class MatrixHttpClient {
+    private static readonly SocketsHttpHandler handler;
+
+    private static readonly HttpClient client;
+
+    static MatrixHttpClient() {
         try {
-            var resp = await hc.GetAsync(url);
-            return resp.IsSuccessStatusCode;
+            handler = new SocketsHttpHandler {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(15),
+                MaxConnectionsPerServer = 4096,
+                EnableMultipleHttp2Connections = true
+            };
+            client = new HttpClient(handler) {
+                DefaultRequestVersion = new Version(3, 0)
+            };
+        }
+        catch (PlatformNotSupportedException e) {
+            Console.WriteLine("Failed to create HttpClient with connection pooling, continuing without connection pool!");
+            Console.WriteLine("Original exception (safe to ignore!):");
+            Console.WriteLine(e);
+
+            client = new HttpClient {
+                DefaultRequestVersion = new Version(3, 0)
+            };
         }
         catch (Exception e) {
-            Console.WriteLine($"Failed to check success status: {e.Message}");
-            return false;
+            Console.WriteLine("Failed to create HttpClient:");
+            Console.WriteLine(e);
+            throw;
         }
     }
-}
 
-public class MatrixHttpClient : HttpClient {
-    public Dictionary<string, string> AdditionalQueryParameters { get; set; } = new();
-    internal string? AssertedUserId { get; set; }
-
+#if SYNC_HTTPCLIENT
     internal SemaphoreSlim _rateLimitSemaphore { get; } = new(1, 1);
-    
-    internal const bool debug = false;
+#endif
+
+    public Dictionary<string, string> AdditionalQueryParameters { get; set; } = new();
+
+    public Uri? BaseAddress { get; set; }
+
+    // default headers, not bound to client
+    public HttpRequestHeaders DefaultRequestHeaders { get; set; } =
+        typeof(HttpRequestHeaders).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, [], null)?.Invoke([]) as HttpRequestHeaders ??
+        throw new InvalidOperationException("Failed to create HttpRequestHeaders");
 
     private JsonSerializerOptions GetJsonSerializerOptions(JsonSerializerOptions? options = null) {
         options ??= new JsonSerializerOptions();
@@ -43,41 +69,41 @@ public class MatrixHttpClient : HttpClient {
     }
 
     public async Task<HttpResponseMessage> SendUnhandledAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
-        if(debug) await _rateLimitSemaphore.WaitAsync(cancellationToken);
-        // Console.WriteLine($"Sending {request.Method} {BaseAddress}{request.RequestUri} ({Util.BytesToString(request.Content?.Headers.ContentLength ?? 0)})");
+#if SYNC_HTTPCLIENT
+        await _rateLimitSemaphore.WaitAsync(cancellationToken);
+#endif
+
+        Console.WriteLine($"Sending {request.Method} {BaseAddress}{request.RequestUri} ({Util.BytesToString(request.Content?.Headers.ContentLength ?? 0)})");
+
         if (request.RequestUri is null) throw new NullReferenceException("RequestUri is null");
         if (!request.RequestUri.IsAbsoluteUri) request.RequestUri = new Uri(BaseAddress, request.RequestUri);
-        // if (AssertedUserId is not null) request.RequestUri = request.RequestUri.AddQuery("user_id", AssertedUserId);
         foreach (var (key, value) in AdditionalQueryParameters) request.RequestUri = request.RequestUri.AddQuery(key, value);
+        foreach (var (key, value) in DefaultRequestHeaders) request.Headers.Add(key, value);
 
-        // Console.WriteLine($"Sending request to {request.RequestUri}");
-
-        try {
-            var webAssemblyEnableStreamingResponseKey =
-                new HttpRequestOptionsKey<bool>("WebAssemblyEnableStreamingResponse");
-            request.Options.Set(webAssemblyEnableStreamingResponseKey, true);
-        }
-        catch (Exception e) {
-            Console.WriteLine("Failed to set browser response streaming:");
-            Console.WriteLine(e);
-        }
+        request.Options.Set(new HttpRequestOptionsKey<bool>("WebAssemblyEnableStreamingResponse"), true);
 
         HttpResponseMessage? responseMessage;
         try {
-            responseMessage = await base.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+            responseMessage = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
         }
         catch (Exception e) {
-            Console.WriteLine($"Failed to send request {request.Method} {BaseAddress}{request.RequestUri} ({Util.BytesToString(request.Content?.Headers.ContentLength ?? 0)}):\n{e}");
+            Console.WriteLine(
+                $"Failed to send request {request.Method} {BaseAddress}{request.RequestUri} ({Util.BytesToString(request.Content?.Headers.ContentLength ?? 0)}):\n{e}");
             throw;
         }
+#if SYNC_HTTPCLIENT
         finally {
-            if(debug) _rateLimitSemaphore.Release();
+            _rateLimitSemaphore.Release();
         }
+#endif
+
+        Console.WriteLine(
+            $"Sending {request.Method} {request.RequestUri} ({Util.BytesToString(request.Content?.Headers.ContentLength ?? 0)}) -> {(int)responseMessage.StatusCode} {responseMessage.StatusCode} ({Util.BytesToString(responseMessage.Content.Headers.ContentLength ?? 0)})");
 
         return responseMessage;
     }
 
-    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+    public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default) {
         var responseMessage = await SendUnhandledAsync(request, cancellationToken);
         if (responseMessage.IsSuccessStatusCode) return responseMessage;
 
@@ -108,8 +134,7 @@ public class MatrixHttpClient : HttpClient {
         if (ex?.RetryAfterMs is null) throw ex!;
         //we have a ratelimit error
         await Task.Delay(ex.RetryAfterMs.Value, cancellationToken);
-        typeof(HttpRequestMessage).GetField("_sendStatus", BindingFlags.NonPublic | BindingFlags.Instance)
-            ?.SetValue(request, 0);
+        request.ResetSendStatus();
         return await SendAsync(request, cancellationToken);
     }
 
@@ -130,20 +155,12 @@ public class MatrixHttpClient : HttpClient {
 
     public async Task<T> GetFromJsonAsync<T>(string requestUri, JsonSerializerOptions? options = null, CancellationToken cancellationToken = default) {
         options = GetJsonSerializerOptions(options);
-        // Console.WriteLine($"GetFromJsonAsync called for {requestUri} with json options {options?.ToJson(ignoreNull:true)} and cancellation token {cancellationToken}");
         var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
         request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         var response = await SendAsync(request, cancellationToken);
         response.EnsureSuccessStatusCode();
         await using var responseStream = await response.Content.ReadAsStreamAsync(cancellationToken);
-#if DEBUG && false // This is only used for testing, so it's disabled by default
-        try {
-            await PostAsync("http://localhost:5116/validate/" + typeof(T).AssemblyQualifiedName, new StreamContent(responseStream), cancellationToken);
-        }
-        catch (Exception e) {
-            Console.WriteLine("[!!] Checking sync response failed: " + e);
-        }
-#endif
+
         return await JsonSerializer.DeserializeAsync<T>(responseStream, options, cancellationToken) ??
                throw new InvalidOperationException("Failed to deserialize response");
     }
@@ -162,9 +179,6 @@ public class MatrixHttpClient : HttpClient {
         options = GetJsonSerializerOptions(options);
         var request = new HttpRequestMessage(HttpMethod.Put, requestUri);
         request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        // Console.WriteLine($"Sending PUT {requestUri}");
-        // Console.WriteLine($"Content: {JsonSerializer.Serialize(value, value.GetType(), options)}");
-        // Console.WriteLine($"Type: {value.GetType().FullName}");
         request.Content = new StringContent(JsonSerializer.Serialize(value, value.GetType(), options),
             Encoding.UTF8, "application/json");
         return await SendAsync(request, cancellationToken);
@@ -190,28 +204,24 @@ public class MatrixHttpClient : HttpClient {
         var result = JsonSerializer.DeserializeAsyncEnumerable<T>(await res.Content.ReadAsStreamAsync(), options);
         await foreach (var resp in result) yield return resp;
     }
+
+    public async Task<bool> CheckSuccessStatus(string url) {
+        //cors causes failure, try to catch
+        try {
+            var resp = await client.GetAsync(url);
+            return resp.IsSuccessStatusCode;
+        }
+        catch (Exception e) {
+            Console.WriteLine($"Failed to check success status: {e.Message}");
+            return false;
+        }
+    }
+
+    public async Task<HttpResponseMessage> PostAsync(string uri, HttpContent? content, CancellationToken cancellationToken = default) {
+        var request = new HttpRequestMessage(HttpMethod.Post, uri) {
+            Content = content
+        };
+        return await SendAsync(request, cancellationToken);
+    }
 }
-
-public class JsonFloatStringConverter : JsonConverter<float> {
-    public override float Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        => float.Parse(reader.GetString()!);
-
-    public override void Write(Utf8JsonWriter writer, float value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
-}
-
-public class JsonDoubleStringConverter : JsonConverter<double> {
-    public override double Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        => double.Parse(reader.GetString()!);
-
-    public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
-}
-
-public class JsonDecimalStringConverter : JsonConverter<decimal> {
-    public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        => decimal.Parse(reader.GetString()!);
-
-    public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
-}
+#endif
